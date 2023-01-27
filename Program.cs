83961f03f1e3d9a5dedcd4a09811147acf1c844d
naman-weybee <naman@weybee.in>
2023-01-27 11:58:02 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using HtmlAgilityPack;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.DevTools;
+using System;
+using System.Net;
+using System.Reflection.Metadata;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Phillips_Crawling
+{
+    class Program
+    {
+        private const string Url = "https://www.phillips.com/auctions/past/filter/Departments%3DWatches/sort/newest";
+
+        static void Main(string[] args)
+        {
+            GetWatchAuctionDetails();
+            Console.ReadLine();
+        }
+
+        private static string GetFullyLoadedWebPageContent(WebDriver driver)
+        {
+            long scrollHeight = 0;
+            IJavaScriptExecutor js = driver;
+            do
+            {
+                var newScrollHeight = (long)js.ExecuteScript("window.scrollTo(0, document.body.scrollHeight); return document.body.scrollHeight;");
+                if (newScrollHeight != scrollHeight)
+                {
+                    scrollHeight = newScrollHeight;
+                    Thread.Sleep(1000);
+                }
+                else
+                {
+                    break;
+                }
+            } while (true);
+            return driver.PageSource;
+        }
+
+        private static void GetWatchAuctionDetails()
+        {
+            WebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(Url);
+
+            var PageSource = GetFullyLoadedWebPageContent(driver);
+        }
+    }
+}
