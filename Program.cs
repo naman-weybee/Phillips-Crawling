4e1fe625b636247194d5fda41769105fd3fdc57e
naman-weybee <naman@weybee.in>
2023-02-01 10:18:33 +0530

    content modified

--- Program.cs
+++ Program.cs
@@ -55,6 +55,7 @@ namespace Phillips_Crawling
         public static async void GetWatchAuctionDetails()
         {
             ChromeOptions opt = new();
+            opt.AddArguments("headless");
             opt.AddArgument("--log-level=3");
             opt.AddArguments("--disable-gpu");
             opt.AddArguments("--start-maximized");
